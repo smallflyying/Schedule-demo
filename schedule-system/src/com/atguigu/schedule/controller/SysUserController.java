31c5a9d422b0835ba2f495424470545e39cce15e
lihongfei <2529574569@qq.com>
2024-05-17 23:46:14 +0800

    日程管理-第四期(注册提交前校验用户名是否占用功能)

--- schedule-system/src/com/atguigu/schedule/controller/SysUserController.java
+++ schedule-system/src/com/atguigu/schedule/controller/SysUserController.java
@@ -21,6 +21,31 @@ public class SysUserController extends BaseController {
 
     private SysUserService userService = new SysUserServiceImpl();
 
+
+    /**
+     * 注册时，接收要注册的用户名，校验用户名是否被占用的业务接口
+     * @param req
+     * @param resp
+     * @throws ServletException
+     * @throws IOException
+     */
+    protected void checkUsernameUsed(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 接收用户名
+        String username = req.getParameter("username");
+
+        // 调用服务层处理方法查询该用户名是否有对应的用户
+        SysUser sysUser = userService.findByUsername(username);
+
+        // 如果有  响应 已占用
+        // 如果没有 响应 可用
+        String info = "可用";
+        if (null != sysUser) {
+            info = "已占用";
+        }
+
+        resp.getWriter().write(info);
+    }
+
     /**
      * 接收用户登录请求，完成的登录业务接口
      * @param req
